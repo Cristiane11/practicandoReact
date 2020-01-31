5b98a5ad1f57605fb873f6179d44d32413c350f4
Cristiane11 <bjwmcd@yahoo.com>
2020-01-31 04:29:49 -0500

    update

---
+++ my-react/src/Projects.js
@@ -0,0 +1,40 @@
+import react from 'react';
+import bikeYellow from './Photos';
+
+export const Projects = [
+    {
+      id: 1,
+      name: 'Robot',
+      skills: 'React',
+      photo:'{bikeYellow}',
+      link: 'www.yahoo.com'
+    },
+    {
+        id: 2,
+        name: 'Rsestaurant',
+        skills: 'React & Node',
+        photo:'',
+        link: 'www.yahoo.com'
+    },
+    {
+        id: 3,
+        name: 'Click Game',
+        skills: 'React & Axios,tachyons',
+        photo:'',
+        link: 'www.yahoo.com'
+    },
+    {
+      id: 4,
+      name: 'Construction',
+      skills: 'React & Axios,tachyons, mongoDB',
+      photo:'',
+      link: 'www.yahoo.com'
+    },
+    {
+      id: 5,
+      name: 'Styling',
+      skills: 'React & Axios,tachyons',
+      photo:'',
+      link: 'www.yahoo.com'
+    }
+  ];
